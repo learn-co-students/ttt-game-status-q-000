ec824a887e2dea0984a1fe099469f4b69dc1f258
neonwires <neonwires@gmail.com>
2015-11-07 20:15:19 +0000

    nitrous-backup at Sat Nov  7 20:15:19 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ]
+
+def won?(board)
+  count_check = -1
+ WIN_COMBINATIONS.each do |combo|
+   count_check += 1
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+      return combo
+    elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      return combo
+    elsif count_check > 8
+      return false
+    end
+  end
+end
