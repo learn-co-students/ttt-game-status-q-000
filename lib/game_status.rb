f8d9b6d8503cebf469365fe46ab21f7bed11a181
tyleresmith <oats000@hotmail.com>
2015-11-08 20:15:19 +0000

    nitrous-backup at Sun Nov  8 20:15:19 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,39 @@
 # Helper Method
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
 def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8], #horizontals
+  [0,3,6],
+  [1,4,7],
+  [2,5,8], #verticals
+  [0,4,8],
+  [6,4,2], #diags
+  ]
+
+def won(board)
+  if board.include?[WIN_COMBINATIONS[]]
+    if WIN_COMBINATIONS == ["X","X","X"]
+      puts "Congratulations! X wins!"
+      elsif WIN_COMBINATIONS == ["O","O","O"]
+      puts "Congratulations, ) wins!"
+    else
+    end
+  else
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
