a07a663313e6321fd57681c6c211d2390883e03b
jessiegibson <jessie@thirstynyc.com>
2015-11-25 21:26:18 -0700

    Finally figured out most of it

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,44 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], 
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |winner|
+    winner.all? {|token| board[token] == "X"} || winner.all?{|token| board[token] == "O"}
+   end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"} 
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+  return true
+  end
+end
+
+def winner(board)
+  if the_index_win = won?(board)
+    board[the_index_win.first]  
+  end
+end
