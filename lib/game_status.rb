fe320bdd5273ed1d9d3f09e4b93ba60f5eb8071e
jart602 <jart3450@yahoo.com>
2015-11-30 07:50:00 +0000

    nitrous-backup at Mon Nov 30 07:49:59 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,10 +4,18 @@ def position_taken?(board, location)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+  WIN_COMBINATIONS.detect do |win_index|
+    position_1 = board[win_index[0]]
+    position_2 = board[win_index[1]]
+    position_3 = board[win_index[2]]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      win_index
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      win_index
+    else
+      false
+    end
   end
 end
 
@@ -28,11 +36,11 @@ def over?(board)
 end
 
 def winner(board)
-  won?(board).all? do |win|
-    board[win] == "X" || board[win] == "O"
+  if won?(board)
+    win_token = won?(board)
+    board[win_token[0]]
   end
 end
-
 
 
 
