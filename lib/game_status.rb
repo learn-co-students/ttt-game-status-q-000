a860d7069f4c30558e0d48c91b3acf320c31ad37
krisnotchris <krishenderson@gmail.com>
2015-12-25 13:47:06 -0600

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,58 @@
-# Helper Method
-def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+   [0, 1, 2],  # Top row
+   [3, 4, 5],  # Middle row
+   [6, 7, 8],  # Bottom row
+   [0, 3, 6],  # Left column
+   [1, 4, 7],  # Middle column
+   [2, 5, 8],  # Right column
+   [0, 4, 8],  # Diagonal 1
+   [2, 4, 6]   # Diagonal 2
+ ]
+ 
+ def won?(board)
+   WIN_COMBINATIONS.each do |win_combo|
+     if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
+       return win_combo
+     elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+       return win_combo
+     end
+   end
+   
+   return false
+ end
+ 
+ def full?(board)
+   board.all?{|position| position == "X" || position == "O"}
+ end
+ 
+ def draw?(board)
+   if !won?(board) && full?(board)
+     true
+   elsif !won?(board) && !full?(board)
+     false
+   elsif won?(board)
+     false
+   end
+ end
+ 
+ def over?(board)
+   if won?(board) || draw?(board) || full?(board)
+     true
+   else
+     false
+   end
+ end
+ 
+ def winner(board)
+   if won?(board)
+     WIN_COMBINATIONS.each do |win_combo|
+       if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
+         return "X"
+       elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+         return "O"
+       end
+     end
+   else
+     nil
+   end
+ end
