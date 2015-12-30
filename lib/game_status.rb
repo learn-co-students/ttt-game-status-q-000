583aede1ccf6c3b6a93c6b2904198ccc2f1b44cb
Chen Yang <yangc5@mail.uc.edu>
2015-12-30 20:38:03 +0000

    nitrous-backup at Wed Dec 30 20:38:03 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -77,3 +77,12 @@ def winner(board)
     end
   end
 end
+
+# code your #valid_move? method here
+def valid_move?(board, position)
+  if position.to_i>=1 && position.to_i<=9 && !position_taken?(board, position.to_i-1)
+    return true
+  else
+    return false
+  end
+end
