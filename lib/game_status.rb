71148958590ca2442621d554797fc598a98d3372
CTAnthny <CTAnthny@gmail.com>
2016-01-02 20:06:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,13 +64,13 @@ end
 
 def winner(board)
   #return "X" or "O" winner
-  if won?(board) == true
-    if won?(board).all? { |nested_position| board[nested_position] == "X" }
-      return "X"
-    elsif won?(board).all? { |nested_position| board[nested_position] == "O" }
-      return "O"
+    if won?(board) == false
+      return nil
+    else
+      if won?(board).all? { |nested_position| board[nested_position] == "X" }
+        return "X"
+      elsif won?(board).all? { |nested_position| board[nested_position] == "O" }
+        return "O"
+      end   
     end
-  elsif won?(board) == false
-    return nil
-  end
 end
