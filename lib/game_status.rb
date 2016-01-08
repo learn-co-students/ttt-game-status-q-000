ec922aeb6b20b7a49fb9a53a9c3303410105c61f
rambllah <adam.bramhall@gmail.com>
2016-01-08 03:27:06 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,31 @@ WIN_COMBINATIONS = [
   [0,4,8], # Top left to bottom right
   [2,4,6]  # Top right to bottom left
 ]
+
+def won?(board)
+    WIN_COMBINATIONS.find do |win_line|
+    board[win_line[0]] == board[win_line[1]] &&
+    board[win_line[1]] == board[win_line[2]] &&
+    win_line.all? { |counter| board[counter] == "X" } || win_line.all? { |counter| board[counter] == "O" }
+  end
+end
+
+def full?(board)
+  board.all? { |counter| counter == "X" || counter == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+  return board[won?(board)[0]]
+  else
+  return nil
+  end
+end
