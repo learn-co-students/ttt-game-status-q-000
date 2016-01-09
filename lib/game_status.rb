b637e8b1959ec018208180d2aea498c3e30b1e39
jakeekaj <jakemalto2014@gmail.com>
2016-01-09 19:57:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,67 @@ def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
+def won?(board)
+  WIN_COMBINATIONS.each do |x|
+    win_index_1 = x[0]
+    win_index_2 = x[1]
+    win_index_3 = x[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return x.to_a
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return x.to_a
+
+  end
+
+ end
+  return nil
+end
+
+def full?(board)
+  !board.any?{|x| x == " " || x == "" }
+end
+
+def draw?(board)
+  !(won?(board)) && full?(board)
+end
+
+def over?(board)
+  won?(board)||full?(board)||draw?(board)
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |x|
+    win_index_1 = x[0]
+    win_index_2 = x[1]
+    win_index_3 = x[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return "X"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return "O"
+
+  end
+ end
+  return nil
+end
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
