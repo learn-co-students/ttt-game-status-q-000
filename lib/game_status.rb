e4bcb80afde91c4848eb9e1092108758a117cce4
andytouch93 <andy.touch.93@icloud.com>
2016-01-24 15:02:32 -0600

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,61 @@ def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
+def won?(board)
+  WIN_COMBINATIONS.each do |chickendinner|
+    if chickendinner.all? {|index| board[index] == "X" }
+      return chickendinner
+    elsif chickendinner.all? {|index| board[index] == "O"}
+      return chickendinner
+    end
+  end
+  false
+end
+
+def full?(board)
+  if board.detect {|spot| spot == " "}
+    false
+  else
+    true
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) || won?(board)
+    true
+  else
+    false
+  end
+end
+
+
+def winner(board)
+  WIN_COMBINATIONS.each do |chickendinner|
+    if chickendinner.all? {|index| board[index] == "X" }
+      return "X"
+    elsif chickendinner.all? {|index| board[index] == "O"}
+      return "O"
+    end
+  end
+  nil
+end
+
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [3,4,5],
+  [6,7,8],
+  [6,4,2]]
