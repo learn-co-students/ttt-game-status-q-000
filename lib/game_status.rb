b137111fb509c85c0789b61d40ef18de0119419a
aunnam <ajit.unnam@berkeley.edu>
2016-01-30 00:40:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,65 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #left col
+  [1,4,7], #middle col
+  [2,5,8], #right col
+  [0,4,8], #forward dag
+  [2,4,6] #backward dag
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+    pos_1 = board[win_index_1]
+    pos_2 = board[win_index_2]
+    pos_3 = board[win_index_3]
+    
+    if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
+      return win_combo
+    elsif pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
+      return win_combo
+    end
+  end
+  return false
+end
+
+def full?(board)
+  count = 0
+  while count < 9
+    if position_taken?(board,count)
+      count+=1
+    else return false
+    end
+  end
+  return true
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    return true
+  else return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    win_combo = won?(board)
+    return board[win_combo[0]]
+  end
+  return nil
+end
