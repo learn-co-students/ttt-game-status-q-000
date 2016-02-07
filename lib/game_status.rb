e67e94d54a7f31c480ebebd1cf6f655703e04422
joketrump <joketrump@gmail.com>
2016-02-07 05:33:17 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,19 +18,19 @@ def full?(board)
 end
 
 def draw?(board)
-  if full?(board) == true
-    if won?(board) != false
-      return false
-    else
-      return true
-    end
+  if full?(board) == true && won?(board) == false
+    return true
+  elsif full?(board) == false
+    return false
+  elsif full?(board) == true && won?(board) != false
+    return false
   end
 end
 
 def over?(board)
-  if draw?(board) == true
+  if won?(board) != false
     return true
-  elsif won?(board) != false
+  elsif draw?(board) == true
     return true
   end
 end
@@ -48,32 +48,26 @@ def winner(board)
   end
 end
 
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 ]
 
 def won?(board)
-  if empty?(board) == true
-    return false
-  else
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combination
-      end
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     end
-    return false
   end
+  return false
 end
 
