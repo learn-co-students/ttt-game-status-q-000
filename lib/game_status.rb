10eef605010b4b1bee642bb8e5bdd7a717007ad0
zaynrahn <zaynrahn@gmail.com>
2016-02-08 04:13:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,45 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row win
+  [3,4,5], #middle row win
+  [6,7,8], #bottom row win
+  [0,3,6], #left column win
+  [1,4,7], #middle column win
+  [2,5,8], #right column win
+  [0,4,8], #topL to bottomR diag win
+  [2,4,6] #topR to bottomL diag win
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    position_1 = board[win_combination[0]]
+    position_2 = board[win_combination[1]]
+    position_3 = board[win_combination[2]]
+
+    if (position_1 == "X" && position_2 =="X" && position_3 =="X") || (position_1 == "O" && position_2 =="O" && position_3 =="O")
+      return win_combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|cell| cell == "X" || cell == "O"}
+end
+
+def draw?(board)
+  !(won?(board)) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
