7a5aba11cdb33b866315179eed84572c3dce4523
KL Tah <kltah1@gmail.com>
2016-02-10 16:58:08 +0000

    nitrous-backup at Wed Feb 10 16:58:08 UTC 2016

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,35 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+#puts win_combination
+
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+  if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    puts win_combination
+    return win_combination # return the win_combination indexes that won.
+  end
+
+  end
+  return false
+end
+
+def full?(board)
+  if (board.detect {|x| x == " "}) == " "
+    return false
+  else return true
+  end
+
+end
