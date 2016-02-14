c7d349b9f63a4388d57a944a15c8a4432bb303d7
cguirgui <cyril.guirguis@gmail.com>
2016-02-14 01:55:31 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,59 @@
-# Helper Method
-def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+WIN_COMBINATIONS =
+  [[0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8]]
+
+
+def won?(board)
+  combo = 0
+  while combo < WIN_COMBINATIONS.length
+    current_combo = WIN_COMBINATIONS[combo]
+
+    win1 = current_combo.all? { |position| board[position] == "X" }
+    win2 = current_combo.all? { |position| board[position] == "O" }
+
+    if win1 == true || win2 == true
+      return current_combo
+    end
+    combo += 1
+  end
+  return nil
 end
 
-# Define your WIN_COMBINATIONS constant
+def winner(board)
+
+  won = won?(board)
+
+  if won != nil
+    return board[won[0]]
+  end
+end
+
+def full?(board)
+  board.all? {|position| position == "X" || position =="O"}
+end
+
+def draw?(board)
+  if full?(board) == true && (won?(board) == false || won?(board) == nil)
+    return true
+  else false
+  end
+end
+
+def over?(board)
+  if draw?(board) == true || won?(board) != nil || full?(board) == true
+    return true
+  end
+end
+
+
+
+
