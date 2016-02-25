529f90c1f16d27b1bbcfe306cb614bab47570251
Salima Bennou <salima.bennou@gmail.com>
2016-02-25 23:58:17 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,53 +1,43 @@
-# Helper Method
-def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
-end
 WIN_COMBINATIONS = [
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6]
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,4,8],
+[2,4,6],
+[0,3,6],
+[1,4,7],
+[2,5,8]
 ]
 
-def won?(board)
-WIN_COMBINATIONS.detect do |win_type|
-position_taken?(board, win_type[0])
-board[win_type[0]] == board[win_type[1]]
-board[win_type[1]] == board[win_type[2]]
+def position_taken?(board, location)
+    !(board[location].nil? || board[location] == " ")
 end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+
+  board[combo[0]] == board[combo[1]] &&
+  board[combo[1]] == board[combo[2]] &&
+  position_taken?(board, combo[0])
+  end
 end
 
 def full?(board)
-  board.none?{|full_board| full_board==" "}
+  board.all? do |token|
+  token == "X" || token == "O"
+  end
 end
 
 def draw?(board)
-  if full?(board) == true && won?(board) == nil
-    return true
-  else
-    return false
-  end
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  if full?(board) == true || won?(board) == true || draw?(board) == true
-     return true
-  else
-     return false
-  end
+  won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
-  if won?(board) == nil
-    return nil
-  else
-    return board[won?(board)[0]]
-  end
+ if winning_combo = won?(board)
+ board[winning_combo.first]
+ end
 end
-
-
-
