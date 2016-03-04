0489ec8b40dab438d628400883b9bd0689cb4501
dannyenfp <daniel.valmas@gmail.com>
2016-03-04 17:29:18 -0500

    test

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,65 @@
 # Helper Method
+
 def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
+ !(board[location].nil? || board[location] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top Row
+  [3,4,5], # Middle Row
+  [6,7,8], # Bottom Row 
+  [0,3,6], # First Column
+  [1,4,7], # Second Column
+  [2,5,8], # Third Column 
+  [0,4,8], # First Diagonal 
+  [2,4,6]  # Second Diagonal
+]
+
+def won?(board) 
+WIN_COMBINATIONS.each do |combination| 
+  pos1 = board[combination[0]]
+  pos2 = board[combination[1]]
+  pos3 = board[combination[2]]
+
+ if (((pos1 == pos2) && (pos2 == pos3)) && (position_taken?(board, combination[0])))
+  return combination
+else
+  false
+end
+end 
+end
+
+def full?(board)
+  board.each do |value| 
+    if (value == "O" || value == "X")
+      true
+    else 
+      break false
+    end
+  end
+end
+
+
+def draw?(board)
+  if (won?(board) == false && full?(board) == true) 
+    true
+  else 
+    false
+  end
+end 
+
+
+#takes in a board and returns 
+def over?(board) 
+  if (won?(board) == true || full?(board) == true)
+    true
+  else 
+    false
+  end
+end 
+
+#def winner(board)
+ # if 
+
+
