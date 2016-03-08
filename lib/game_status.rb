895d3526ab9a32e936a4c93b74636352d9550f56
layups159 <amauris.quezada@gmail.com>
2016-03-08 18:25:32 +0000

    nitrous-backup at Tue Mar  8 18:25:32 UTC 2016

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
-board = ["X", "O", "X", "O", "X", "O", "O", "O", "X"]
+
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
