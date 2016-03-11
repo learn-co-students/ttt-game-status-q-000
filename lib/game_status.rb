5b1a82c416ee82ec8bbacc36b0262d654f1920b0
Certain125 <amanita.certain@gmail.com>
2016-03-11 18:27:59 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,8 +22,6 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return single_win
-    else
-      return false
     end
   end
 end
