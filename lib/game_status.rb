15338712a13edcf826038181a9782bfeedf1839c
Certain125 <amanita.certain@gmail.com>
2016-03-11 17:23:58 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,35 @@
+
+
 # Helper Method
 def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6],
+  [1,4,7], [2,5,8], [0,4,8], [6,4,2]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |single_win|
+    win_index_1 = single_win[0]
+    win_index_2 = single_win[1]
+    win_index_3 = single_win[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return single_win
+    end
+  end
+end
+
+def full?(board)
+  #all positions are taken = true
+  #returns false for an inprogress game
+  #returns true for a draw
+end
+
+
