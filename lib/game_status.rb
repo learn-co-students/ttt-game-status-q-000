50ce174e0a3a6154faba58f294bb69280066cbff
MichaelChen1120 <michael.chen.1120@gmail.com>
2016-03-22 19:08:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,37 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[2,5,8],[1,4,7]]
+
+def won?(board)
+
+
+
+  top_row_win = [0,1,2]
+  middle_row_win = [3,4,5]
+  bottom_row_win = [6,7,8]
+  left_diagonal_win =[0,4,8]
+  right_diagonal_win =[2,4,6]
+  left_column_win = [0,3,6]
+  right_column_win = [2,5,8]
+  middle_column_win = [1,4,7]
+
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+    return WIN_COMBINATIONS[0]
+    elsif board[middle_row_win[0]] == "X" && board[middle_row_win[1]] == "X" && board[middle_row_win[2]] == "X"
+    return WIN_COMBINATIONS[1]
+    elsif board[bottom_row_win[0]] == "X" && board[bottom_row_win[1]] == "X" && board[bottom_row_win[2]] == "X"
+    return WIN_COMBINATIONS[2]
+    elsif board[left_diagonal_win[0]] == "X" && board[left_diagonal_win[1]] == "X" && board[left_diagonal_win[2]] == "X"
+    return WIN_COMBINATIONS[3]
+    elsif board[right_diagonal_win[0]] == "X" && board[right_diagonal_win[1]] == "X" && board[right_diagonal_win[2]] == "X"
+    return WIN_COMBINATIONS[4]
+    elsif board[left_column_win[0]] == "X" && board[left_column_win[1]] == "X" && board[left_column_win[2]] == "X"
+    return WIN_COMBINATIONS[5]
+    elsif board[right_column_win[0]] == "X" && board[right_column_win[1]] == "X" && board[right_column_win[2]] == "X"
+    return WIN_COMBINATIONS[6]
+    elsif board[middle_column_win[0]] == "X" && board[middle_column_win[1]] == "X" && board[middle_column_win[2]] == "X"
+    return WIN_COMBINATIONS[7]
+  end
+end
