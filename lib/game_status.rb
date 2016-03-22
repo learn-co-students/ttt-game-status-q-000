62d288a2fa161ad5167b9432af4c8b209bea29b1
Terry-Thompson <terry.lyn.thompson@gmail.com>
2016-03-22 12:45:51 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,31 @@
 # Helper Method
+require 'pry'
 def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], #top row
+[3,4,5], #middle row
+[6,7,8], # bottom row
+[0,3,6], #first column
+[1,4,7], #second column
+[2,5,8], #third column
+[0,4,8], #first diagonal
+[2,4,6], #second diagonal
+]
+
+
+
+def won?(board)
+   WIN_COMBINATIONS.each do |wins|
+    wins.any?  do |positions| 
+      binding.pry
+      positions.all?  positions == "X" || positions == "O" ? wins : false
+  end
+ end
+end
+    
+
+
