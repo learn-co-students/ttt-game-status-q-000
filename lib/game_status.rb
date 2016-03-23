c6c80d657bed119220100e544620368c0d4d5871
MichaelChen1120 <michael.chen.1120@gmail.com>
2016-03-23 18:00:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,12 +55,9 @@ def over?(board)
   end
 end
 
+
 def winner(board)
-  if won?(board) && WIN_COMBINATIONS.each {|string| string == "X"}
-     "X"
-   elsif won?(board) && WIN_COMBINATIONS.each {|string| string == "O"}
-     "O"
-    else
-    nil
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
