9f8a591d75e808974103664e8b50dd07c51a27c6
dpreli <dave.preli@gmail.com>
2016-03-30 18:43:40 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,87 +1,42 @@
 
 def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
-end
+   !(board[location].nil? || board[location] == " ")
+ end 
 
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[6,4,2]
-]
-
-def won?(board)
-
-  WIN_COMBINATIONS.each do |win_combination|
-
-
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]  
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    else
-      false 
-    end
-  end 
-  exit = false
-  exit
-end
-
-
-
-def full?(board)
-  board.none?{|j| j == " "}
-end
-
-def draw?(board)
-  if won?(board) != false
-    false
-  elsif board.none?{|k| k == " "}
-    true
-  end
-end  
+ WIN_COMBINATIONS = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [6,4,2]
+ ]
+ 
+ def won?(board)
+   WIN_COMBINATIONS.detect do |combo|
+     board[combo[0]] == board[combo[1]] &&
+     board[combo[1]] == board[combo[2]] &&
+     position_taken?(board, combo[0])
+   end
+ end
+ 
+ def full?(board)
+   board.all?{|token| token == "X" || token == "O"}
+ end
+ 
+ def draw?(board)
+   full?(board) && !won?(board)
+ end
 
 def over?(board)
-  if draw?(board) == true || full?(board) == true || won?(board) == true
-    true
-  else
-    false
-  end
-end
+  won?(board) || full?(board)
+ end
 
 def winner(board)
-  WIN_COMBINATIONS.each do |win_combination|
-
-
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]  
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return "X"
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return "O"
-    else
-      nil 
-    end
-  end 
-  exit = nil
-  exit
+   if winning_combo = won?(board)
+    board[winning_combo.first]
+ end
 end
-
 
