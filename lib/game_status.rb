01f9f42a1a902d65d584c6561f2d97a26216e133
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-04-05 05:08:25 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,55 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], #TOP ROW WIN
+  [3,4,5], #MID ROW WIN
+  [6,7,8], #BOT ROW WIN   #  0 | 1 | 2
+  [0,3,6], #LEFT WIN      #  ---------
+  [1,4,7], #MID WIN       #  3 | 4 | 5
+  [2,5,8], #RIGHT WIN     #  ---------
+  [0,4,8], #L2R WIN       #  6 | 7 | 8
+  [2,4,6]  #R2L WIN
+
+]
+
+#WON METHOD
+
+def won?(board)
+ WIN_COMBINATIONS.detect do |sequence|
+    board[sequence[0]] == board[sequence[1]] &&
+    board[sequence[0]] == board[sequence[2]] &&
+ 	position_taken?(board, sequence[0])
+  end
+end
+
+#FULL METHOD
+
+def full?(board)
+   board.all? do |characters|
+   characters == "X" || characters == "O"
+  end 
+end
+
+#DRAW METHOD
+
+def draw?(board)
+    full?(board) && !won?(board)
+end
+
+#OVER METHOD
+def over?(board)
+	full?(board) == true
+end
+
+#WINNER METHOD
+def winner(board)
+   if winning_combo = won?(board)
+   board[winning_combo.first]
+  end
+end 
+
+
+
+
