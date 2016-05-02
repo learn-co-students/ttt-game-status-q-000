63874437b8067cbb3665587393a6bf87b26d218c
beccagotch <becca.gotch@archiv8.com>
2016-05-02 19:10:00 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+  WIN_COMBINATIONS = [#indexes that compose top row win
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,4,8], # diagonal 
+  [2,4,6], # diagonal 
+  [0,3,6], # left vertical
+  [1,4,7], # middle vertical
+  [2,5,8], # right vertical 
+]
+
+  
+
+#def full?
+
+
+#def draw?
+
+
+#def over?
+
+
+#def winner
