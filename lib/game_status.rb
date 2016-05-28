732d4dcbc9d9f2483263ab085947147f9301079c
Brad Watson <brad.watson.orlando@gmail.com>
2016-05-28 14:57:54 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,27 +15,30 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
-
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      a_win = [position_1, position_2, position_3].all? do |position|
-        position == "X"
-      end
-
-      if a_win
-        return win_combination
-      else
-        false
-      end
+  # Flag / Switch
+  the_answer = false
+  WIN_COMBINATIONS.each do |win_combination| # Scope Gate
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    # implementation
+    # x_win = [position_1, position_2, position_3].all? do |position|
+    #   position == "X"
+    # end
+    # o_win = [position_1, position_2, position_3].all? do |position|
+    #   position == "O"
+    # end
+    a_win = [position_1, position_2, position_3].uniq.size == 1 && position_1 != " "
+    # return value
+    if a_win
+      the_answer = win_combination
     end
   end
-
+  the_answer
+end
 
 def full?(board)
   full_board = board.all? do |space|
@@ -59,5 +62,16 @@ def over?(board)
     true
   else
     false
+  end
+end
+
+def winner(board)
+  winning_arr = won?(board)
+  if winning_arr.class == Array && board[winning_arr[0]] == "X"
+    return "X"
+  elsif winning_arr.class == Array && board[winning_arr[0]] == "O"
+    return "O"
+  else
+    return nil
   end
 end
