ed8a06550a689c1b90a010e42b48dec1ce6b59b4
Lukas Langer <lukas.langer@sportimpuls.at>
2016-06-11 10:54:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,16 +12,41 @@ WIN_COMBINATIONS = [
     [1,4,7], #middle row
     [2,5,8], #right row
     [0,4,8], #cross row
-    [2,4,6] #cross row
+    [6,4,2] #cross row
   ]
 def won?(board)
+  #require 'pry'
+  # binding.pry
     WIN_COMBINATIONS.each do |position|
       if board[position[0]] == "X" && board[position[1]] == "X" && board[position[2]] == "X"
           return position
       elsif board[position[0]] == "O" && board[position[1]] == "O" && board[position[2]] == "O"
           return position
-      else
-          return false
       end
     end
+    false
+end
+
+
+def full?(board)
+  !board.any?{|field| field == " "}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |position|
+    if board[position[0]] == "X" && board[position[1]] == "X" && board[position[2]] == "X"
+        return "X"
+    elsif board[position[0]] == "O" && board[position[1]] == "O" && board[position[2]] == "O"
+        return "O"
+    end
+  end
+  nil
 end
