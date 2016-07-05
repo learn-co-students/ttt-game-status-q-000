997c1d2c125233b9b5de0a2691d5f82bf24381fe
HustleLin <jlin8293@gmail.com>
2016-07-04 23:02:58 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,64 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+
+  # Helper Method
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+  end
+
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8],
+  ]
+
+  def won?(board)
+    WIN_COMBINATIONS.each do |win|
+      if board[win[0]] != " " && board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]] 
+       return win
+  end
+    end
+    false
+  end
+
+  def full?(board)
+    board.each do |position|
+      if position == " "
+        return false
+      end
+    end
+    else return true
+  end
+
+  def draw?(board)
+    if board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?(board)
+    if full?(board) == true
+      return true
+    else
+    return false
+  end
 end
 
-# Define your WIN_COMBINATIONS constant
+def winner(board)
+  if board == ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+    return "X"
+  elsif
+    board == ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+    return "O"
+  else
+    board == ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
+    return nil
+  end
+end
