39018671c2f03ce9184cf831d069830a291ea0a2
KerryAlsace <kerrydougherty@gmail.com>
2016-07-06 17:46:17 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,7 @@ def won?(board)
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-    # return false until position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-
-    # return [win_index_1, win_index_2, win_index_3].to_a
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return [win_index_1, win_index_2, win_index_3]
     else
       false
