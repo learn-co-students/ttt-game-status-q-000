c5526d174abecf41798d7ee7b238cdb6ccd6fcfd
Shane Boyar <shane.boyar@gmail.com>
2016-07-28 17:42:18 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-	WIN_COMBINATIONS.each do |win_combination|
+	WIN_COMBINATIONS.detect do |win_combination|
 		if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
 			return win_combination
   		else
