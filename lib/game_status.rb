c6af3f4e8f0f09194dcfc3169c1c04a3a4245538
smithwebtek <brad@smithwebtek.com>
2018-04-11 22:41:01 -0400

    Refactor winner method

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[6,4,2],[0,4,8],]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
+	WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
@@ -27,7 +27,7 @@ def over?(board)
 end
 
 def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+  if won?(board)
+    board[won?(board).first]
   end
 end
