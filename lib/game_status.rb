2ce3308acf03a452883881d516293ffbdd4be79b
Laura Correa <laura.correa@digitalunit.berlin>
2019-01-28 20:44:51 +0100

    Done 2

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,45 +13,36 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [2,4,6]
-  ]
+]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") #|| (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return win_combination     # return the win_combination indexes that won.
-    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return win_combination
+  WIN_COMBINATIONS.each do |wc|
+    i1 = wc[0]
+    i2 = wc[1]
+    i3 = wc[2]
+    if board[i1] == "X" && board[i2] == "X" && board[i3] == "X" 
+      return wc
+    elsif board[i1] == "O" && board[i2] == "O" && board[i3] == "O" 
+      return wc
     end
   end
   return false
 end
 
 def full?(board)
-  board.all? {|x| x == "X" || x == "O"}
+    board.all?{|e| e == "X" || e == "O"}
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+    full?(board) && !won?(board)
 end
 
 def over?(board)
-  won?(board) || draw?(board)
+  draw?(board) || won?(board)
 end
 
 def winner(board)
-  if won?(board)
-  x =  won?(board) #[0,4,8]
-  y = x[2].to_i
-  board[y]
-  else
-    nil
+  if arr = won?(board)
+    return board[arr[0]]
   end
 end
